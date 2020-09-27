6f4365ac40afdce3375aa0c936039d8cc4561da6
PorsborgTheProgrammer <mads.porsborg.hermansen@gmail.com>
2020-09-27 16:54:04 +0200

    Ballmovement(body alignment)

--- SlopeyFunRun/Assets/Scripts/AlignRotationToTerrainNormal.cs
+++ SlopeyFunRun/Assets/Scripts/AlignRotationToTerrainNormal.cs
@@ -6,28 +6,19 @@ public class AlignRotationToTerrainNormal : MonoBehaviour
 {
     public LayerMask terrainLayers;
     public Transform skinToAlign;
-    // Start is called before the first frame update
-
-    // Update is called once per frame
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, 1.01f);
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         ContactPoint[] contactpoints = new ContactPoint[collision.contactCount];
         collision.GetContacts(contactpoints);
-        skinToAlign.rotation = Quaternion.LookRotation(Vector3.Cross(skinToAlign.right, contactpoints[0].normal));
+        skinToAlign.rotation = Quaternion.LookRotation(skinToAlign.forward, contactpoints[0].normal);
     }
 
     private void OnCollisionStay(Collision collision)
     {
         ContactPoint[] contactpoints = new ContactPoint[collision.contactCount];
         collision.GetContacts(contactpoints);
-        skinToAlign.rotation = Quaternion.LookRotation(skinToAlign.forward, contactpoints[0].normal);
+        skinToAlign.rotation = Quaternion.LookRotation(Vector3.Cross(skinToAlign.right, contactpoints[0].normal), contactpoints[0].normal);
     }
 
 }
